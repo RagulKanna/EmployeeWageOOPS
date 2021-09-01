fd7197459a44f5b46814c6fe3e89f5a36c6a9009
RagulKanna <ragulragi619@gmail.com>
2021-09-01 13:08:08 +0530

    Added:Calculate partime employee wage

--- src/Employeewage/Employeewage.java
+++ src/Employeewage/Employeewage.java
@@ -3,28 +3,43 @@ package Employeewage;
 public class Employeewage
 {
     public static int check=(int)(Math.random()*2);
+    int checkfullhours = (int) (Math.floor(Math.random() * 8 % 9) + 1);
+    int checkparthours = (int) (Math.floor(Math.random() * 4 % 5) + 1);
+    public static int WAGE_PER_HOUR = 20,FULL_DAY_HOUR = 8,PART_TIME_HOUR=4;
+
+
+    public void fulltimeemployeePresentorAbesnt()
+    {
+        System.out.println("\n\ncheck the fulltime employee present or absent:");
+        employeePresentorAbesnt();
+        employeewage(FULL_DAY_HOUR,checkfullhours);
+    }
+
+    public void parttimeemployeewage()
+    {
+        System.out.println("\n\ncheck the parttime employee present or absent:");
+        employeePresentorAbesnt();
+        employeewage(PART_TIME_HOUR,checkparthours);
+
+    }
+
     public void employeePresentorAbesnt()
     {
-
-        System.out.println("\n\ncheck the employee present or absent:");
-
         if(check == 1)
             System.out.println("\n  employee is present today");
         else
             System.out.println("\n  employee is absent today");
     }
 
-    public void dailyemployeewage()
+
+    public void employeewage(int hour,int present)
     {
-        int WAGE_PER_HOUR = 20;
-        int FULL_DAY_HOUR = 8;
-        System.out.println("\n\ncheck the employee daily wage:");
-        int fullpresent = WAGE_PER_HOUR * FULL_DAY_HOUR;
-        System.out.println("\n  if employee worker is present for full day, the salary is " + fullpresent);
+        System.out.println("\n\ncheck the employee wage:");
+        int fullpresent = WAGE_PER_HOUR * hour;
+        System.out.println("\n  if employee worker is present for full hour, the salary is " + fullpresent);
         if (check == 1) {
-            int checkhours = (int) (Math.floor(Math.random() * 8 % 9) + 1);
-            int hoursbased = checkhours * WAGE_PER_HOUR;
-            System.out.println("\n  employee's salary based on hours, employee present for " + checkhours + " hours so his salary is " + hoursbased);
+            int hoursbased = present * WAGE_PER_HOUR;
+            System.out.println("\n  employee's salary based on hours, employee present for " + present + " hours so his salary is " + hoursbased);
         } else
             System.out.println("\n  the employee's salary is 0, because employee is absent");
     }
@@ -35,7 +50,7 @@ public class Employeewage
     System.out.println("\n        Welcome to Employee Wage Computation Program");
 
     Employeewage emp=new Employeewage();
-    emp.employeePresentorAbesnt();
-    emp.dailyemployeewage();
+    emp.fulltimeemployeePresentorAbesnt();
+    emp.parttimeemployeewage();
     }
 }
