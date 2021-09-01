98c5cecd342a1c30cfabbfe6b02510c0ae5c3e52
RagulKanna <ragulragi619@gmail.com>
2021-09-01 12:13:48 +0530

    Added:Check Employee Present or Absent

--- src/Employeewage/Employeewage.java
+++ src/Employeewage/Employeewage.java
@@ -2,7 +2,20 @@ package Employeewage;
 
 public class Employeewage {
 
+    public void EmployeePresentorAbesnt()
+    {
+        int check=(int)(Math.random()*2);
+        System.out.println("\n\n check the employee present or absent:");
+
+        if(check == 1)
+            System.out.println("\nemployee is present today");
+        else
+            System.out.println("\nemployee is absent today");
+    }
     public static void main(String[] args) {
     System.out.println("\n        Welcome to Employee Wage Computation Program");
+
+    Employeewage emp=new Employeewage();
+    emp.EmployeePresentorAbesnt();
     }
 }
