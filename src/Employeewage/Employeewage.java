f01dce016659cda59b5e855497eeb78ec5c6c817
RagulKanna <ragulragi619@gmail.com>
2021-09-01 13:31:15 +0530

    Added:Calcilate monthlywage of employee

--- src/Employeewage/Employeewage.java
+++ src/Employeewage/Employeewage.java
@@ -7,7 +7,7 @@ public class Employeewage
     public static int check=(int)(Math.random()*2);
     int checkfullhours = (int) (Math.floor(Math.random() * 8 % 9) + 1);
     int checkparthours = (int) (Math.floor(Math.random() * 4 % 5) + 1);
-    public static int WAGE_PER_HOUR = 20,FULL_DAY_HOUR = 8,PART_TIME_HOUR=4;
+    public static int WAGE_PER_HOUR = 20,FULL_DAY_HOUR = 8,PART_TIME_HOUR=4,FULL_MONTH_HOUR=100,PART_MONTH_HOUR=50;
 
 
     public void fulltimeemployeePresentorAbesnt()
@@ -15,6 +15,7 @@ public class Employeewage
         System.out.println("\n\ncheck the fulltime employee present or absent:");
         employeePresentorAbesnt();
         employeewage(FULL_DAY_HOUR,checkfullhours);
+        monthlywage(FULL_MONTH_HOUR,WAGE_PER_HOUR);
     }
 
     public void parttimeemployeewage()
@@ -22,6 +23,7 @@ public class Employeewage
         System.out.println("\n\ncheck the parttime employee present or absent:");
         employeePresentorAbesnt();
         employeewage(PART_TIME_HOUR,checkparthours);
+        monthlywage(PART_MONTH_HOUR,WAGE_PER_HOUR);
 
     }
 
@@ -36,7 +38,7 @@ public class Employeewage
 
     public void employeewage(int hour,int present)
     {
-        System.out.println("\n\ncheck the employee wage:");
+        System.out.println("\n\ncheck the employee daily wage:");
         int fullpresent = WAGE_PER_HOUR * hour;
         System.out.println("\n  if employee worker is present for full hour, the salary is " + fullpresent);
         if (check == 1) {
@@ -46,6 +48,12 @@ public class Employeewage
             System.out.println("\n  the employee's salary is 0, because employee is absent");
     }
 
+    public void monthlywage(int hours,int wage)
+    {
+        System.out.println("\n\ncheck the employee monthly wage:");
+        int monthlysalary=hours*wage;
+    System.out.println("\n  Employee's monthly salary is "+monthlysalary);
+    }
 
     public static void main(String[] args)
     {
@@ -56,7 +64,8 @@ public class Employeewage
 
     System.out.println("\nenter the employee to check attendance and wage:");
     System.out.println("\n  Enter\n  1 for FullTime Employee\n  2 for PartTime Employee");
-    int choice=sc.nextInt();
+    System.out.println("\n  your choice: ");int choice=sc.nextInt();
+
         switch (choice)
         {
         case 1:
