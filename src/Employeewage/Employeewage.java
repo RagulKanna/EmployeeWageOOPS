b2cf1515e50f4ee0d11c3c8ae4eb22fe3cd11252
RagulKanna <ragulragi619@gmail.com>
2021-09-01 12:01:52 +0530

    Added:Welcome program

---
+++ src/Employeewage/Employeewage.java
@@ -0,0 +1,8 @@
+package Employeewage;
+
+public class Employeewage {
+
+    public static void main(String[] args) {
+    System.out.println("\n        Welcome to Employee Wage Computation Program");
+    }
+}
